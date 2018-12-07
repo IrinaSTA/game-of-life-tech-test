324e8fe123b2e2cd6147dae80a05e11771d402ea
Irina Baldwin <2007inb@gmail.com>
2018-12-07 12:57:08 +0000

    Pass cell can die and come alive

--- spec/cellSpec.js
+++ spec/cellSpec.js
@@ -10,4 +10,16 @@ describe("Cell", function() {
     cell = new Cell(0)
     expect(cell.state).toEqual(0)
   })
+
+  it("can die", function() {
+    cell = new Cell(1)
+    cell.changeState()
+    expect(cell.state).toEqual(0)
+  })
+
+  it("can come alive", function() {
+    cell = new Cell(0)
+    cell.changeState()
+    expect(cell.state).toEqual(1)
+  })
 })
--- src/cell.js
+++ src/cell.js
@@ -1,3 +1,12 @@
 function Cell(state) {
   this.state = state
 }
+
+// 0 for dead, 1 for alive
+Cell.prototype.changeState = function() {
+  if (this.state === 0) {
+    this.state = 1
+  } else {
+    this.state = 0
+  }
+}
