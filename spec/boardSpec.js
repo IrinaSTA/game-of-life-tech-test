33114f6a28bc72b003f26164cf56119adb652f95
Irina Baldwin <2007inb@gmail.com>
2018-12-07 13:06:35 +0000

    Pass Board has cells and has initial state with some live and some dead cells

--- spec/boardSpec.js
+++ spec/boardSpec.js
@@ -2,11 +2,26 @@ describe('Board', function() {
   var board;
 
   beforeEach(function() {
-    board = new Board(2, 3)
+    function mockCell(x, y, state) {
+      this.x = x
+      this.y = y
+      this.state = state
+    }
+    board2by3 = new Board(2, 3, mockCell)
+    board5by5 = new Board(5, 5, mockCell)
   })
 
   it("can have dimensions", function() {
-    expect(board.x).toEqual(2)
-    expect(board.y).toEqual(3)
+    expect(board2by3.x).toEqual(2)
+    expect(board2by3.y).toEqual(3)
+  })
+
+  it("has cells", function() {
+    expect(board2by3.cells.length).toEqual(6)
+  })
+
+  it("can set its initial state \
+  with some live and some dead cells", function() {
+    expect(board5by5.cells.filter(obj => obj.state).length).not.toEqual(0)
   })
 })
--- src/board.js
+++ src/board.js
@@ -1,4 +1,22 @@
-function Board(x, y) {
+// x for x-axis/width, y for y-axis/height
+function Board(x, y, cellClass = Cell) {
   this.x = x
   this.y = y
+  this.cellClass = cellClass
+  this.cells = (typeof this.cells === 'undefined') ? this._setUpBoard() : this.cells;
+}
+
+// sets up board with live and dead cells chosen at random
+Board.prototype._setUpBoard = function(style = _random) {
+  initial_state = []
+  for (yStep = 1; yStep <= this.y; yStep++) {
+    for (xStep = 1; xStep <= this.x; xStep++) {
+      initial_state.push(new this.cellClass(xStep, yStep, style()))
+    }
+  }
+  return initial_state
+}
+
+function _random() {
+  return Math.round(Math.random())
 }
